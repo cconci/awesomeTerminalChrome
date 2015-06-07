5fb8b1ed4189c1063f9cb6a871a5a6248fa20d1a
Chris <chris@localhost.BigPond>
2015-06-08 00:26:51 +1000

    can now send hex data out and receive it, data is shown and entered in hex

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ function arrayAlementsToString(arrayData) {
   var output = "";
   
   for(i=0;i<arrayData.byteLength;i++) {
-    output += padByteString(arrayData[i]+"")+" ";
+    output += padByteString(arrayData[i].toString(16))+" "; //.toString(16); turns our int to a hex string
     
   }
   
@@ -168,5 +168,46 @@ function padByteString(stringData) {
   else
     return stringData;  
   
+}
 
+function hexStringToByteArray(hexString) {
+  
+  /*
+  Convertes a string like 'AB 00 01 22 FF 10'
+  
+  to a Byte array
+  
+  var byteBuffer = new ArrayBuffer(9);
+  var byteBufferView   = new Int8Array(byteBuffer);
+    
+  byteBufferView[0] = 0xAB;
+  byteBufferView[1] = 0x00;
+  byteBufferView[2] = 0x01;
+  byteBufferView[3] = 0x22;
+  byteBufferView[4] = 0xFF;
+  byteBufferView[5] = 0x10;
+  byteBufferView[6] = '\n';
+  */
+  
+  
+  //step one sepeate the string data, explode on spaces
+  var splitHexString = hexString.split(' ');
+  
+  var byteBuffer = new ArrayBuffer(splitHexString.length);
+  //can not edit the ArrayBuffer, need to go through this method
+  var byteBufferView   = new Int8Array(byteBuffer);
+  
+  for(i=0;i<splitHexString.length;i++) {
+    
+    //form each byte 
+    //http://www.w3schools.com/jsref/jsref_parseint.asp
+    var byte = parseInt(splitHexString[i],'16'); //it is a hex string, thus 16
+    
+    byteBufferView[i] = byte;
+    
+    console.log(byte +'\n');
+    
+  }
+  
+  return byteBuffer;
 }
--- userInteraction.js
+++ userInteraction.js
@@ -8,25 +8,32 @@ document.querySelector('#buttonInputTX').addEventListener("click", function() {
     //sends data out the open port
     //document.querySelector('#heading').innerHTML = "TX";
     
-    var byteBuffer = new ArrayBuffer(9);
+    //debug hardcoded send
+    //var byteBuffer = new ArrayBuffer(9);
     
     //cant edit the ArrayBuffer, ned to og through this method
-    var byteBufferView   = new Int8Array(byteBuffer);
+    //var byteBufferView   = new Int8Array(byteBuffer);
     
-    byteBufferView[0] = 0x40;
-    byteBufferView[1] = 0x41;
-    byteBufferView[2] = 0x42;
-    byteBufferView[3] = 0x43;
-    byteBufferView[4] = 0x44;
-    byteBufferView[5] = 0x45;
-    byteBufferView[6] = 0x46;
-    byteBufferView[7] = 0x47;
-    byteBufferView[8] = '\n';
+    //byteBufferView[0] = 0x40;
+    //byteBufferView[1] = 0x41;
+    //byteBufferView[2] = 0x42;
+    //byteBufferView[3] = 0x43;
+    //byteBufferView[4] = 0x44;
+    //byteBufferView[5] = 0x45;
+    //byteBufferView[6] = 0x46;
+    //byteBufferView[7] = 0x47;
+    //byteBufferView[8] = '\n';
     
-    send_data(byteBuffer);
+    //send_data(byteBuffer);
     
     //add text in the user input as a row in the output
     if( (document.querySelector('#termInput').value).length > 0) {
+      
+      //convert the data in the termInput into a byteBuffer
+      var byteBuffer = hexStringToByteArray((document.querySelector('#termInput').value));
+      
+      send_data(byteBuffer);
+      
       document.querySelector('#termTX').value += (document.querySelector('#termInput').value +"\n");
       
       //auto scroll
