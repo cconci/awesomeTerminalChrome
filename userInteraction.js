38a20ffd4d233a41fe31fcf91ad3479e083174f4
cconci <cconci.dev@gmail.com>
2015-06-23 23:08:22 +1000

    added button to hide UI sections, but have left them in active (they do not look good), cleaned up some formatting in userInteraction.js

--- userInteraction.js
+++ userInteraction.js
@@ -5,74 +5,74 @@ cconci
 
 document.querySelector('#buttonInputTX').addEventListener("click", function() {
     
-    //sends data out the open port
-    //document.querySelector('#heading').innerHTML = "TX";
+  //sends data out the open port
+  //document.querySelector('#heading').innerHTML = "TX";
+  
+  //debug hardcoded send
+  //var byteBuffer = new ArrayBuffer(9);
+  
+  //cant edit the ArrayBuffer, ned to og through this method
+  //var byteBufferView   = new Int8Array(byteBuffer);
+  
+  //byteBufferView[0] = 0x40;
+  //byteBufferView[1] = 0x41;
+  //byteBufferView[2] = 0x42;
+  //byteBufferView[3] = 0x43;
+  //byteBufferView[4] = 0x44;
+  //byteBufferView[5] = 0x45;
+  //byteBufferView[6] = 0x46;
+  //byteBufferView[7] = 0x47;
+  //byteBufferView[8] = '\n';
+  
+  //send_data(byteBuffer);
+  
+  //add text in the user input as a row in the output
+  if( (document.querySelector('#termInput').value).length > 0) {
     
-    //debug hardcoded send
-    //var byteBuffer = new ArrayBuffer(9);
+    //convert the data in the termInput into a byteBuffer
+    var byteBuffer = hexStringToByteArray((document.querySelector('#termInput').value));
     
-    //cant edit the ArrayBuffer, ned to og through this method
-    //var byteBufferView   = new Int8Array(byteBuffer);
+    send_data(byteBuffer);
     
-    //byteBufferView[0] = 0x40;
-    //byteBufferView[1] = 0x41;
-    //byteBufferView[2] = 0x42;
-    //byteBufferView[3] = 0x43;
-    //byteBufferView[4] = 0x44;
-    //byteBufferView[5] = 0x45;
-    //byteBufferView[6] = 0x46;
-    //byteBufferView[7] = 0x47;
-    //byteBufferView[8] = '\n';
+    document.querySelector('#termTX').value += (arrayAlementsToString(new Uint8Array(byteBuffer)) +"\n");
     
-    //send_data(byteBuffer);
+    //auto scroll
+    var ta = document.getElementById('termTX');
+    ta.scrollTop = ta.scrollHeight;
     
-    //add text in the user input as a row in the output
-    if( (document.querySelector('#termInput').value).length > 0) {
-      
-      //convert the data in the termInput into a byteBuffer
-      var byteBuffer = hexStringToByteArray((document.querySelector('#termInput').value));
-      
-      send_data(byteBuffer);
-      
-      document.querySelector('#termTX').value += (arrayAlementsToString(new Uint8Array(byteBuffer)) +"\n");
-      
-      //auto scroll
-      var ta = document.getElementById('termTX');
-      ta.scrollTop = ta.scrollHeight;
-      
-    }
+  }
 });
 document.querySelector('#buttonInputClear').onclick = function() {
     
-    //Clears the textbox
-    document.querySelector('#termInput').value = "";
+  //Clears the textbox
+  document.querySelector('#termInput').value = "";
 };
 document.querySelector('#buttonTXClear').onclick = function() {
     
-    //Clears the textbox
-    document.querySelector('#termTX').value = "";
+  //Clears the textbox
+  document.querySelector('#termTX').value = "";
     
-    txByteCount = 0;
+  txByteCount = 0;
     
-    updateStatsCounters();
+  updateStatsCounters();
 };
 document.querySelector('#buttonRXClear').onclick = function() {
     
-    //Clears the textbox
-    document.querySelector('#termRX').value = "";
+  //Clears the textbox
+  document.querySelector('#termRX').value = "";
     
-    //filter counter
-    rxFilterXNumberOfBytesCount = 0;
+  //filter counter
+  rxFilterXNumberOfBytesCount = 0;
     
-    //byte count
-    rxByteCount = 0;
+  //byte count
+  rxByteCount = 0;
     
-    updateStatsCounters();
+  updateStatsCounters();
 };
 document.querySelector('#buttonRefreshPorts').onclick = function() {
     
-    //
-    update_ports();
+  //
+  update_ports();
 };
 
 document.querySelector('#baudRatesList').onchange = function() {
@@ -91,27 +91,27 @@ document.querySelector('#baudRatesList').onchange = function() {
 
 document.querySelector('#buttonAdvancedOptions').onclick = function() {
     
-    //
-    if(document.querySelector('#divAdvancedOptions').style.display === "") {
-      document.querySelector('#divAdvancedOptions').style.display = "none";
-    }
-    else {
-      document.querySelector('#divAdvancedOptions').style.display = "";
-      //change button text to hide advanced options etc...
-    }
+  //
+  if(document.querySelector('#divAdvancedOptions').style.display === "") {
+    document.querySelector('#divAdvancedOptions').style.display = "none";
+  }
+  else {
+    document.querySelector('#divAdvancedOptions').style.display = "";
+    //change button text to hide advanced options etc...
+  }
     
 };
 
 document.querySelector('#buttonRXOptions').onclick = function() {
     
-    //
-    if(document.querySelector('#divRXOptions').style.display === "") {
-      document.querySelector('#divRXOptions').style.display = "none";
-    }
-    else {
-      document.querySelector('#divRXOptions').style.display = "";
-      //change button text to hide advanced options etc...
-    }
+  //
+  if(document.querySelector('#divRXOptions').style.display === "") {
+    document.querySelector('#divRXOptions').style.display = "none";
+  }
+  else {
+    document.querySelector('#divRXOptions').style.display = "";
+    //change button text to hide advanced options etc...
+  }
     
 };
 
@@ -137,20 +137,20 @@ document.querySelector('#buttonFontN').onclick = function() {
 
 document.querySelector('#buttonConnectToPort').onclick = function() {
     
-    //
-    connect_to_port();
+  //
+  connect_to_port();
 };
 
 
 document.querySelector('#UIOptions').onclick = function() {
     
-    if(document.querySelector('#divUIOptions').style.display === "") {
-      document.querySelector('#divUIOptions').style.display = "none";
-    }
-    else {
-      document.querySelector('#divUIOptions').style.display = "";
-      //change button text to hide advanced options etc...
-    }  
+  if(document.querySelector('#divUIOptions').style.display === "") {
+    document.querySelector('#divUIOptions').style.display = "none";
+  }
+  else {
+    document.querySelector('#divUIOptions').style.display = "";
+    //change button text to hide advanced options etc...
+  }  
     
 };
 
